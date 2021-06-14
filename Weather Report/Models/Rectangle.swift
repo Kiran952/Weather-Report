45ebdce2abaf0c450af4d1a5d66986de314c629c
Arun Amuri <arun.amuri@dnata.com>
2021-06-14 19:28:29 +0400

    added rectangular file to understand mutating

---
+++ Weather Report/Models/Rectangle.swift
@@ -0,0 +1,50 @@
+//
+//  Rectangle.swift
+//  Weather Report
+//
+//  Created by Arun Amuri on 14/06/2021.
+//
+
+import Foundation
+import SwiftUI
+
+//mutating - which can be modified
+//immutatble - which cant be modified - struct is an immutable
+
+struct Rectangle {
+    //properties
+    var width: Float
+    var height: Float
+    var area: Float  = 0.0
+    
+    init(width: Float, height: Float) {
+        self.width = width
+        self.height = height
+    }
+    //mutating is not required becuse it is a struct - value type
+    mutating func calculateArea() {
+        area = height * width
+    }
+    
+    func calculateAreaV2() -> Float {
+        return width * height
+    }
+}
+
+class Square {
+    //properties
+    var width: Float
+    var height: Float
+    var area: Float = 0.0
+    
+    
+    init(width: Float, height: Float) {
+        self.width = width
+        self.height = height
+    }
+    //mutating is not required becuse it is a class - reference type
+    func calculateArea() {
+        area = width * height
+    }
+}
+
